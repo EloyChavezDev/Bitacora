d4bfb0b073c05ad24f779f167367930ea354eac5
EloyChavezDev <laneloychavez@gmail.com>
2024-01-28 21:48:35 -0600

    Update config.ts
    Creo no funcionaba mi nueva variable porque no la tenia declarara aqui en el TS

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
+    progress: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
